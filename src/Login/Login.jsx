397a8745f617e1d604f1db92d5f671ed09a7bdad
nusrat3657 <nusratjahan3657r@gmail.com>
2024-04-08 08:17:18 +0600

    added show icon

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -4,6 +4,8 @@ import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
 import { FaFacebookF, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Login = () => {
@@ -11,6 +13,8 @@ const Login = () => {
     // const location = useLocation();
     // const navigate = useNavigate();
     // console.log('location in the login page', location);
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleLogin = e => {
         e.preventDefault();
@@ -49,7 +53,18 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text text-lg font-semibold">Password</span>
                         </label>
-                        <input type="password" name="password" placeholder="Enter your  password" className="input input-bordered" required />
+                        <div className="relative">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                placeholder="Enter your password"
+                                className="input input-bordered w-full" required />
+                            <span className="absolute top-4 -ml-8" onClick={() => setShowPassword(!showPassword)}>
+                                {
+                                    showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                                }
+                            </span>
+                        </div>
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover text-md font-semibold mt-2">Forgot password?</a>
                         </label>
@@ -58,7 +73,7 @@ const Login = () => {
                         <button className="btn bg-[#23BE0A] text-white font-semibold text-lg">Login</button>
                     </div>
                 </form>
-                <p className="font-semibold text-center">Don't have an account? <Link to='/register' className="text-red-500">Register</Link></p>
+                <p className="font-semibold text-center">Don't have an account? Please<Link to='/register' className="text-red-500">Register</Link></p>
                 <div className='p-2  mt-6 flex justify-evenly'>
                     <button className="btn btn-outline hover:bg-[#23BE0A]">
                         <FaGoogle></FaGoogle>
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,30 +1,54 @@
 // import { useContext } from "react";
+// import { createUser } from "firebase/auth";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import { ToastContainer, toast } from 'react-toastify';
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Register = () => {
     // const { createUser } = useContext(AuthContext)
 
-    // const handleRegister = e => {
-    //     e.preventDefault();
-    //     console.log(e.currentTarget);
-    //     const form = new FormData(e.currentTarget);
-    //     const name = form.get('name');
-    //     const photo = form.get('photo');
-    //     const email = form.get('email');
-    //     const password = form.get('password');
-    //     console.log(name, photo, email, password);
+    const [showPassword, setShowPassword] = useState(false);
 
-    //     // create user
-    //     createUser(email, password)
-    //         .then(result => {
-    //             console.log(result.user);
-    //         })
-    //         .catch(error => {
-    //             console.error(error);
-    //         })
-    // }
+    const handleRegister = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name, photo, email, password);
+
+        // if (password.length < 6) {
+        //     toast.warn('Password  must be at least 6 characters or longer');
+        //     return;
+        // }
+        // else if (!/[A-Z]/.test(password)) {
+        //     toast.warn('Password must have an Uppercase letter');
+        //     return;
+        // }
+        // else if (!/[a-z]/.test(password)) {
+        //     toast.warn('Password must have an Lowercase letter');
+        //     return;
+        // }
+
+
+
+        // create user
+        // createUser(email, password)
+        //     .then(result => {
+        //         console.log(result.user);
+        //         toast.success('User created successfully');
+        //     })
+        //     .catch(error => {
+        //         console.error(error);
+        //         toast.error('Something went wrong, please try again');
+        //     })
+    }
 
     return (
         <div className="">
@@ -32,7 +56,7 @@ const Register = () => {
             <div className="bg-[#23BE0A]/20 rounded-lg md:w-3/4 lg:w-1/2 mx-auto p-10 my-10">
                 <h2 className="text-4xl my-2 text-center font-bold  font-league">Register Your Account</h2>
                 <hr />
-                <form  className="card-body ">
+                <form onSubmit={handleRegister} className="card-body ">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text text-lg font-semibold">Your Name</span>
@@ -51,18 +75,30 @@ const Register = () => {
                         </label>
                         <input type="email" name="email" placeholder="Enter your email address" className="input input-bordered" required />
                     </div>
-                    <div className="form-control">
+                    <div className="form-control ">
                         <label className="label">
                             <span className="label-text text-lg font-semibold">Password</span>
                         </label>
-                        <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
+                        <div className="relative">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                placeholder="Enter your password"
+                                className="input input-bordered w-full" required />
+                            <span className="absolute top-4 -ml-8" onClick={() => setShowPassword(!showPassword)}>
+                                {
+                                    showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                                }
+                            </span>
+                        </div>
                     </div>
                     <div className="form-control mt-6">
                         <button className="btn bg-[#23BE0A] text-white font-semibold text-lg">Register</button>
                     </div>
                 </form>
-                <p className="font-semibold text-center">Already have an account? <Link to='/login' className="text-red-500">Login</Link></p>
+                <p className="font-semibold text-center">Already have an account? Please <Link to='/login' className="text-red-500">Login</Link></p>
             </div>
+            {/* <ToastContainer /> */}
         </div>
     );
 };
